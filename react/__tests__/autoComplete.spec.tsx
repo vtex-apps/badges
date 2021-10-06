f4bb24ba43123031efb5173dca34435347729990
Ana Maia <ana.baptistao@vtex.com.br>
2021-10-06 15:46:49 -0300

    testes autocomplete

--- react/__tests__/autoComplete.spec.tsx
+++ react/__tests__/autoComplete.spec.tsx
@@ -6,7 +6,7 @@ import AutoComplete from '../utils/autoComplete'
 // eslint-disable-next-line jest/no-mocks-import
 import { values } from '../__mocks__/values'
 
-describe('AutComplete Area', () => {
+describe('AutoComplete Area', () => {
   it('should render the auto complete', async () => {
     const onChange = jest.fn()
     const value = { id: 'id' }
@@ -14,10 +14,34 @@ describe('AutComplete Area', () => {
 
     const { container } = render(
       <Context.Provider value={values}>
-        <AutoComplete onChange={onChange()} name={name} value={value} />
+        <AutoComplete
+          onChange={onChange()}
+          name={name}
+          value={value}
+          label={'Marca'}
+        />
       </Context.Provider>
     )
 
     expect(container.firstChild).not.toBeNull()
+  })
+
+  it('should render the auto complete with nameProduct null', async () => {
+    const onChange = jest.fn()
+    const value = { id: 'id' }
+    const name = 'productId'
+
+    const { getAllByTestId } = render(
+      <Context.Provider value={values}>
+        <AutoComplete
+          onChange={onChange()}
+          name={name}
+          value={value}
+          label={'Produto'}
+        />
+      </Context.Provider>
+    )
+
+    expect(getAllByTestId('button-modal-close')[0]).toContainHTML('button')
   })
 })
