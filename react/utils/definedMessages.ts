05463e5d0314bbd7a541af766602385de15416f1
Ana Maia <ana.baptistao@vtex.com.br>
2022-03-24 14:51:38 -0300

    Add admin/ antes das mensagens

--- react/utils/definedMessages.ts
+++ react/utils/definedMessages.ts
@@ -1,156 +1,156 @@
 import { defineMessages } from 'react-intl'
 
 export const alert = defineMessages({
-  save: { id: 'badges.data.save' },
-  error: { id: 'badges.data.save.errors' },
+  save: { id: 'admin/badges.data.save' },
+  error: { id: 'admin/badges.data.save.errors' },
 })
 
 export const button = defineMessages({
-  title: { id: 'badges.button.title' },
-  image: { id: 'badges.button.image' },
-  text: { id: 'badges.button.text' },
-  html: { id: 'badges.button.html' },
-  description: { id: 'badges.button.description' },
+  title: { id: 'admin/badges.button.title' },
+  image: { id: 'admin/badges.button.image' },
+  text: { id: 'admin/badges.button.text' },
+  html: { id: 'admin/badges.button.html' },
+  description: { id: 'admin/badges.button.description' },
 })
 
 export const buttonSave = defineMessages({
-  save: { id: 'badges.button.save' },
+  save: { id: 'admin/badges.button.save' },
 })
 
 export const conditions = defineMessages({
-  rule: { id: 'badges.conditions.rule' },
-  selectRule: { id: 'badges.conditions.select.rule' },
-  headerPrefix: { id: 'badges.conditions.header.prefix' },
-  all: { id: 'badges.conditions.all' },
-  none: { id: 'badges.conditions.none' },
-  headerSufix: { id: 'badges.conditions.header.sufix' },
-  new: { id: 'badges.conditions.new' },
-  and: { id: 'badges.conditions.and' },
-  or: { id: 'badges.conditions.or' },
+  rule: { id: 'admin/badges.conditions.rule' },
+  selectRule: { id: 'admin/badges.conditions.select.rule' },
+  headerPrefix: { id: 'admin/badges.conditions.header.prefix' },
+  all: { id: 'admin/badges.conditions.all' },
+  none: { id: 'admin/badges.conditions.none' },
+  headerSufix: { id: 'admin/badges.conditions.header.sufix' },
+  new: { id: 'admin/badges.conditions.new' },
+  and: { id: 'admin/badges.conditions.and' },
+  or: { id: 'admin/badges.conditions.or' },
 })
 
 export const file = defineMessages({
-  label: { id: 'badges.file.label' },
-  dropImage: { id: 'badges.file.drop.image' },
-  choiceImage: { id: 'badges.file.choice.image' },
-  acceptFiles: { id: 'badges.file.accept.files' },
+  label: { id: 'admin/badges.file.label' },
+  dropImage: { id: 'admin/badges.file.drop.image' },
+  choiceImage: { id: 'admin/badges.file.choice.image' },
+  acceptFiles: { id: 'admin/badges.file.accept.files' },
 })
 
 export const input = defineMessages({
-  namePlaceholder: { id: 'badges.input.name.placeholder' },
-  nameLabel: { id: 'badges.input.name.label' },
-  textPlaceholder: { id: 'badges.input.text.placeholder' },
-  textLabel: { id: 'badges.input.text.label' },
+  namePlaceholder: { id: 'admin/badges.input.name.placeholder' },
+  nameLabel: { id: 'admin/badges.input.name.label' },
+  textPlaceholder: { id: 'admin/badges.input.text.placeholder' },
+  textLabel: { id: 'admin/badges.input.text.label' },
 })
 
 export const html = defineMessages({
-  label: { id: 'badges.html.label' },
+  label: { id: 'admin/badges.html.label' },
 })
 
 export const edit = defineMessages({
-  name: { id: 'badges.edit.name' },
-  style: { id: 'badges.edit.style' },
-  textShowRows: { id: 'badges.edit.text.show.rows' },
-  textOf: { id: 'badges.edit.text.of' },
-  priority: { id: 'badges.edit.priority' },
-  edit: { id: 'badges.edit.edit' },
-  delete: { id: 'badges.edit.delete' },
+  name: { id: 'admin/badges.edit.name' },
+  style: { id: 'admin/badges.edit.style' },
+  textShowRows: { id: 'admin/badges.edit.text.show.rows' },
+  textOf: { id: 'admin/badges.edit.text.of' },
+  priority: { id: 'admin/badges.edit.priority' },
+  edit: { id: 'admin/badges.edit.edit' },
+  delete: { id: 'admin/badges.edit.delete' },
 })
 
 export const provider = defineMessages({
-  errorName: { id: 'badges.provider.error.name' },
-  errorImage: { id: 'badges.provider.error.image' },
-  errorText: { id: 'badges.provider.error.text' },
-  errorHtml: { id: 'badges.provider.error.html' },
-  errorSimpleStatement: { id: 'badges.provider.error.simple.statement' },
-  sucessEdit: { id: 'badges.provider.sucess.edit' },
-  errorEdit: { id: 'badges.provider.error.edit' },
-  sucessDelete: { id: 'badges.provider.sucess.delete' },
-  errorDelete: { id: 'badges.provider.error.delete' },
-  errorPriority: { id: 'badges.provider.error.priority' },
+  errorName: { id: 'admin/badges.provider.error.name' },
+  errorImage: { id: 'admin/badges.provider.error.image' },
+  errorText: { id: 'admin/badges.provider.error.text' },
+  errorHtml: { id: 'admin/badges.provider.error.html' },
+  errorSimpleStatement: { id: 'admin/badges.provider.error.simple.statement' },
+  sucessEdit: { id: 'admin/badges.provider.sucess.edit' },
+  errorEdit: { id: 'admin/badges.provider.error.edit' },
+  sucessDelete: { id: 'admin/badges.provider.sucess.delete' },
+  errorDelete: { id: 'admin/badges.provider.error.delete' },
+  errorPriority: { id: 'admin/badges.provider.error.priority' },
 })
 
 export const options = defineMessages({
-  category: { id: 'badges.options.category' },
-  brand: { id: 'badges.options.brand' },
-  product: { id: 'badges.options.product' },
-  collection: { id: 'badges.options.collection' },
-  specification: { id: 'badges.options.specification' },
-  sku: { id: 'badges.options.sku' },
-  is: { id: 'badges.options.is' },
-  isNot: { id: 'badges.options.not.is' },
+  category: { id: 'admin/badges.options.category' },
+  brand: { id: 'admin/badges.options.brand' },
+  product: { id: 'admin/badges.options.product' },
+  collection: { id: 'admin/badges.options.collection' },
+  specification: { id: 'admin/badges.options.specification' },
+  sku: { id: 'admin/badges.options.sku' },
+  is: { id: 'admin/badges.options.is' },
+  isNot: { id: 'admin/badges.options.not.is' },
 })
 
 export const badgessArea = defineMessages({
-  title: { id: 'badges.badges.area.title' },
-  labelTab1: { id: 'badges.badges.area.tab.1.label' },
-  labelTab2: { id: 'badges.badges.area.tab.2.label' },
+  title: { id: 'admin/badges.badges.area.title' },
+  labelTab1: { id: 'admin/badges.badges.area.tab.1.label' },
+  labelTab2: { id: 'admin/badges.badges.area.tab.2.label' },
 })
 
 export const commonModal = defineMessages({
   save: {
-    id: 'badges.modal.save',
+    id: 'admin/badges.modal.save',
   },
   cancel: {
-    id: 'badges.modal.cancel',
+    id: 'admin/badges.modal.cancel',
   },
   delete: {
-    id: 'badges.modal.delete',
+    id: 'admin/badges.modal.delete',
   },
   edit: {
-    id: 'badges.modal.edit',
+    id: 'admin/badges.modal.edit',
   },
   ok: {
-    id: 'badges.modal.ok',
+    id: 'admin/badges.modal.ok',
   },
 })
 
 export const modalDelete = defineMessages({
   deleteText: {
-    id: 'badges.modal.delete.text',
+    id: 'admin/badges.modal.delete.text',
   },
   deleteQuestion: {
-    id: 'badges.modal.delete.question',
+    id: 'admin/badges.modal.delete.question',
   },
 })
 
 export const modalEdit = defineMessages({
   editText: {
-    id: 'badges.modal.edit.text',
+    id: 'admin/badges.modal.edit.text',
   },
   changeImage: {
-    id: 'badges.modal.edit.change.image',
+    id: 'admin/badges.modal.edit.change.image',
   },
 })
 
 export const specification = defineMessages({
-  inputName: { id: 'badges.specification.input.name' },
-  inputValue: { id: 'badges.specification.input.value' },
+  inputName: { id: 'admin/badges.specification.input.name' },
+  inputValue: { id: 'admin/badges.specification.input.value' },
 })
 
 export const priority = defineMessages({
-  title: { id: 'badges.priority.title' },
-  description: { id: 'badges.priority.description' },
-  one: { id: 'badges.priority.one' },
-  two: { id: 'badges.priority.two' },
-  three: { id: 'badges.priority.three' },
-  four: { id: 'badges.priority.four' },
-  five: { id: 'badges.priority.five' },
+  title: { id: 'admin/badges.priority.title' },
+  description: { id: 'admin/badges.priority.description' },
+  one: { id: 'admin/badges.priority.one' },
+  two: { id: 'admin/badges.priority.two' },
+  three: { id: 'admin/badges.priority.three' },
+  four: { id: 'admin/badges.priority.four' },
+  five: { id: 'admin/badges.priority.five' },
 })
 
 export const autocomplete = defineMessages({
-  placeholder: { id: 'badges.autocomplete.placeholder' },
-  text: { id: 'badges.autocomplete.text' },
+  placeholder: { id: 'admin/badges.autocomplete.placeholder' },
+  text: { id: 'admin/badges.autocomplete.text' },
 })
 
 export const modalError = defineMessages({
-  textError: { id: 'badges.modal.error.text' },
-  text: { id: 'badges.modal.error.text2' },
+  textError: { id: 'admin/badges.modal.error.text' },
+  text: { id: 'admin/badges.modal.error.text2' },
 })
 
 export const buttonOptionsMessages = defineMessages({
-  errorImage: { id: 'badges.button.option.error.image' },
-  errorText: { id: 'badges.button.option.error.text' },
-  errorHtml: { id: 'badges.button.option.error.html' },
-  errorScript: { id: 'badges.button.option.error.script' },
+  errorImage: { id: 'admin/badges.button.option.error.image' },
+  errorText: { id: 'admin/badges.button.option.error.text' },
+  errorHtml: { id: 'admin/badges.button.option.error.html' },
+  errorScript: { id: 'admin/badges.button.option.error.script' },
 })
