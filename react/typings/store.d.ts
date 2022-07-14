8b934236ce0ea32633140a748c5755801ac8ca68
beatrizmaselli <beatriz.maselli@vtex.com.br>
2022-07-14 12:47:32 -0300

    fix product context

--- react/typings/store.d.ts
+++ react/typings/store.d.ts
@@ -8,28 +8,5 @@ interface PropsStore {
   text?: unknown
   children?: ReactNode
   page?: string
-  productQuery?: ProductQueryProps
   numberOfBadges?: number
-}
-
-interface ProductQueryProps {
-  product: ProductProps
-}
-
-interface ProductProps {
-  brandId: number
-  categoryId: string
-  productId: string
-  productClusters: ClusterProps[]
-  properties: PropertiesProps[]
-}
-
-interface ClusterProps {
-  id: string
-  name: string
-}
-
-interface PropertiesProps {
-  name: string
-  values: string[]
 }
